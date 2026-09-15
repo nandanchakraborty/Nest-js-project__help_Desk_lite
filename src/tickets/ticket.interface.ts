965703fe6277ea02a6456e770a5a42a4a437b0e5
nandanchakraborty <nandancsebubt@gmail.com>
2026-09-15 19:37:28 +0600

    initial setup

---
+++ src/tickets/ticket.interface.ts
@@ -0,0 +1,11 @@
+import { StringifyOptions } from "querystring";
+
+export interface Ticket {
+    id:number;
+    subject:string;
+    description:string;
+    priority:'low'|'medium'|'high';
+    status:'open'|'close';
+    createdAt:string;
+
+}
