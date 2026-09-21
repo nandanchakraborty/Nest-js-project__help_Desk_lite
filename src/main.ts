1b51ce93115af2ac4b79f2595ad985c0cdfa0bd9
nandanchakraborty <nandancsebubt@gmail.com>
2026-09-21 11:37:25 +0600

    added interceptor to format every successful reponse

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { ResponseInterceptor } from './common/response.interceptor.js';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(`api`);
@@ -10,6 +11,7 @@ async function bootstrap() {
       forbidNonWhitelisted:true,
     }),
   );
+  app.useGlobalInterceptors(new ResponseInterceptor)
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
